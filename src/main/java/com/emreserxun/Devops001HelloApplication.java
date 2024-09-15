09a7ef2b5153f3c98995a7eae07e4dcd1499c2e5
Emre Kanlıbaş <74323128+serxun@users.noreply.github.com>
2024-09-15 15:04:32 -0700

    devops_001_

---
+++ src/main/java/com/emreserxun/Devops001HelloApplication.java
@@ -0,0 +1,13 @@
+package com.emreserxun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops001HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops001HelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emreserxun/Devops001HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreserxun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops001HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
